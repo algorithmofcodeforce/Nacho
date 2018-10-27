36067b02c87141be2825de189b5b0313f8527acc
KookminNacho <39543045+KookminNacho@users.noreply.github.com>
2018-10-27 19:55:53 +0900

    F

--- 3036.py
+++ 3036.py
@@ -2,30 +2,13 @@ Count = int(input())
 i = []
 i = list(input().split())
 
-ilist = []
-for j in range(len(i)):
-    i[j] = int(i[j])
-for j in range(len(i)):
-    ilist.append(i[j])
-for j in range(len(i)):
-    i[j] = int(i[j])
+for k in range(1,Count):
+    gcd = []
+    for j in range(1,int(i[k])+1):
+        if int(i[0]) % j == 0 and int(i[k]) % j == 0:
+            gcd.append(int(j))
 
-for num in range(1,Count):
-    for j in range(len(ilist)):
-        i[j] = ilist[j]
-    if i[0] % i[num] == 0:
-        print(i[0]//i[num],end='/1')
-        print("")
-        ######
-    elif i[0] % i[num] != 0:
-        k = 2
-        while k < i[0]and k < i[num]:
-            if i[0]%k ==0 and i[num]%k==0:
-                i[0] = i[0]//k
-                i[num] = i[num]//k
-            else:
-                k += 1
-        print(i[0],end='/')
-        print(i[num])
-
-##채점시 틀렸따고 뜸..
+    num = int(max(gcd))
+    x = str(int(i[0]) // num)
+    y = str(int(i[k]) // num)
+    print(x+"/"+y)
